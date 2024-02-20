c7a5d961d225403d260c2f1aa6711e2d86dfc83f
openmpy <suhwan@kakao.com>
2024-02-20 11:09:11 +0900

    feat: 선택한 게시글 조회 기능
    - 게시글 조회 테스트 코드(성공, 실패)

--- src/test/java/com/example/mission01/domain/service/BoardServiceTest.java
+++ src/test/java/com/example/mission01/domain/service/BoardServiceTest.java
@@ -1,7 +1,9 @@
 package com.example.mission01.domain.service;
 
+import com.example.mission01.domain.dto.BoardReadResponseDto;
 import com.example.mission01.domain.dto.BoardWriteRequestDto;
 import com.example.mission01.domain.dto.BoardWriteResponseDto;
+import com.example.mission01.domain.entity.Board;
 import com.example.mission01.domain.repository.BoardRepository;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
@@ -11,7 +13,10 @@ import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 
+import java.util.Optional;
+
 import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.anyLong;
 import static org.mockito.Mockito.when;
 
 @ExtendWith(MockitoExtension.class)
@@ -39,5 +44,35 @@ class BoardServiceTest {
         Assertions.assertEquals("제목", responseDto.getTitle());
         Assertions.assertEquals("홍길동", responseDto.getWriter());
         Assertions.assertEquals("내용", responseDto.getContents());
+    }
+
+    @Test
+    @DisplayName("게시글을 조회한다.")
+    void read_01() throws Exception {
+        // given
+        Board board = Board.builder()
+                .id(1L)
+                .title("제목")
+                .contents("내용")
+                .password("1234")
+                .build();
+
+        // stub
+        when(boardRepository.findById(anyLong())).thenReturn(Optional.of(board));
+
+        // when
+        BoardReadResponseDto responseDto = boardService.read(board.getId());
+
+        // then
+        Assertions.assertEquals(1, responseDto.getId());
+        Assertions.assertEquals("제목", responseDto.getTitle());
+        Assertions.assertEquals("내용", responseDto.getContents());
+    }
+
+    @Test
+    @DisplayName("실패 - 찾을 수 없는 게시글을 조회한다.")
+    void read_02() throws Exception {
+        // when & then
+        Assertions.assertThrows(RuntimeException.class, () -> boardService.read(1L));
     }
 }
